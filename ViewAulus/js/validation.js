ff7ffdb52a665bc7dcefb4d9dd4d389d914e894b
johnatannunes <31327291+johnatannunes@users.noreply.github.com>
2017-10-01 00:00:25 +0100

    rawmaterial
    reconstruçao

---
+++ ViewAulus/js/validation.js
@@ -0,0 +1,9 @@
+﻿function validation(name) {
+
+    if (document.form.name.value == "") {
+        alert("Please Write here");
+        document.form.name.focus();
+        return false;
+    }
+}
+
