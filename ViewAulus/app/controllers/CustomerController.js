72ef64d720fdbcc41eb089c1ac96faebfde958a2
LENOVO-PC\LENOVOO <gerson_mata@hotmail.com>
2017-09-30 21:43:18 +0100

    Start AngularJs
    Init in the page customer

---
+++ ViewAulus/app/controllers/CustomerController.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="D:\Projects\PlasticManufacturer\ViewAulus\Scripts/angular.min.js" />
+
+angular.module('plasticManufacturer')
+    .controller("CustomerController", function ($scope, $http) {
+
+        $http.get("welcome.htm").then(function (response) {
+            $scope.customer = response.data;
+        });
+
+    });
