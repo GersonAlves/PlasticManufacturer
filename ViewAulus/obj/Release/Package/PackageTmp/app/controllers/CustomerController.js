46701ee600b43b002111be95cc990a29faf6268b
johnatannunes <31327291+johnatannunes@users.noreply.github.com>
2017-10-16 19:36:01 +0100

    responsive
    color-match-request

---
+++ ViewAulus/obj/Release/Package/PackageTmp/app/controllers/CustomerController.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="D:\Projects\PlasticManufacturer\ViewAulus\Scripts/angular.min.js" />
+
+angular.module('plasticManufacturer')
+    .controller("CustomerController", function ($scope, $http) {
+
+        $http.get("http://localhost:55751/api/Customers").then(function (response) {
+            $scope.Customers = response.data;
+        });
+
+    });
