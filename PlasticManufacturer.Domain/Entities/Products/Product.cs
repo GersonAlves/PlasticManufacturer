a11eb129f3cc275b25eea2b63fbc756ed9641010
tiagoalvesmata <tiagoalvesmata@gmail.com>
2017-11-25 18:15:25 +0000

    Alteracao Object Produto

--- PlasticManufacturer.Domain/Entities/Products/Product.cs
+++ PlasticManufacturer.Domain/Entities/Products/Product.cs
@@ -8,6 +8,10 @@ namespace PlasticManufacturer.Domain.Entities.Products
 {
     public class Product: Entity
     {
+        public virtual string Code { get; set; }
+        public virtual string Ratio { get; set; }
+        public virtual bool Compound { get; set; }
+        public virtual decimal Price { get; set; }
         public virtual Formula Formula { get; set; }
     }
 }
